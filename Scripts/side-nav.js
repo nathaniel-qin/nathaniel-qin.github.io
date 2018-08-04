03224c710f72c504662bbf17d504e760d9e89446
Nathaniel Qin <nathaniel.qin1@gmail.com>
2018-08-04 16:48:28 -0700

    added side navigation

---
+++ Scripts/side-nav.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+
+  $(".side-nav-opt").hover(
+    function() {
+      $(this).animate({
+        backgroundColor: "rgb(240, 240, 240)"
+      }, 175);
+    }, function() {
+      $(this).stop(true).animate({
+        backgroundColor: "white"
+      }, 175);
+    }
+  );
+
+});
